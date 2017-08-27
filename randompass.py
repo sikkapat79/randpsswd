8f86fbce1df994d196ac3b2e8f6f534d27830456
Sikkapat Sricheangsa <bank.s.sricheangsa@gmail.com>
2017-08-27 19:49:39 +0700

    First Commit

---
+++ randompass.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import argparse
+import random
+parser = argparse.ArgumentParser()
+
+def main():
+    '''
+    This is main function.
+    '''
+    parser.add_argument("-n")
+    parser.add_argument("-c")
+    args = parser.parse_args()
+
+    psswd = ''
+    psswd_len = int(args.n or 12)
+    possible_char = args.c or "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXoxYyZz0123456789!@$%"
+    possible_len = len(possible_char) - 1
+
+    for i in range(0, psswd_len):
+        char_index = random.randint(0, possible_len)
+        psswd += possible_char[char_index]
+
+    print "Your password is : {0}".format(psswd)
+
+main()
